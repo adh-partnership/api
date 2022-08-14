d337af5c8820fb3a32b51b542e75224de900dc41
Daniel Hawton <daniel@hawton.org>
2022-08-13 18:54:06 -0600

    sluggify util func

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,5 +1,10 @@
 package utils
 
+import (
+	"regexp"
+	"strings"
+)
+
 func ArrayContains(array []string, item string) bool {
 	for _, a := range array {
 		if a == item {
@@ -7,4 +12,18 @@ func ArrayContains(array []string, item string) bool {
 		}
 	}
 	return false
+}
+
+func StringToSlug(s string) string {
+	if len(s) > 100 {
+		s = s[:99]
+	}
+	s = strings.ToLower(s)
+	re := regexp.MustCompile(`[^a-z0-9 -]+`)
+	s = re.ReplaceAllString(s, "")
+	s = strings.Replace(s, " ", "-", -1)
+	s = strings.Replace(s, "--", "-", -1)
+	s = strings.TrimRight(s, "-")
+	s = strings.TrimSpace(s)
+	return s
 }
--- pkg/utils/utils_test.go
+++ pkg/utils/utils_test.go
@@ -10,3 +10,48 @@ func TestArrayContains(t *testing.T) {
 	assert.True(t, ArrayContains([]string{"a", "b", "c"}, "b"))
 	assert.False(t, ArrayContains([]string{"a", "b", "c"}, "d"))
 }
+
+func TestStringToSlug(t *testing.T) {
+	tests := []struct {
+		Name           string
+		Input          string
+		ExpectedOutput string
+	}{
+		{
+			Name:           "Empty",
+			Input:          "",
+			ExpectedOutput: "",
+		},
+		{
+			Name:           "Short",
+			Input:          "Hello",
+			ExpectedOutput: "hello",
+		},
+		{
+			Name:           "Long",
+			Input:          "Hello, World!",
+			ExpectedOutput: "hello-world",
+		},
+		{
+			Name:           "Longer",
+			Input:          "Hello, World! This is a very long string.",
+			ExpectedOutput: "hello-world-this-is-a-very-long-string",
+		},
+		{
+			Name: "Long with truncate",
+			Input: "non odio euismod lacinia at quis risus sed vulputate odio ut enim blandit volutpat maecenas volutpat blandit aliquam etiam erat" +
+				" velit scelerisque in dictum",
+			ExpectedOutput: "non-odio-euismod-lacinia-at-quis-risus-sed-vulputate-odio-ut-enim-blandit-volutpat-maecenas-volutpa",
+		},
+		{
+			Name:           "Test with special characters that should get filtered out",
+			Input:          "Hello+!@%$(!% World",
+			ExpectedOutput: "hello-world",
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.Name, func(t *testing.T) {
+			assert.Equal(t, test.ExpectedOutput, StringToSlug(test.Input))
+		})
+	}
+}
