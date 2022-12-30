28393927f89b2f24410564982963fe694acc99a3
Daniel Hawton <daniel@hawton.org>
2022-12-29 20:33:50 -0700

    lint

--- internal/v1/airport/main.go
+++ internal/v1/airport/main.go
@@ -6,8 +6,6 @@ import (
 	"github.com/adh-partnership/api/pkg/logger"
 )
 
-var log = logger.Logger.WithField("component", "admin")
-
 func Routes(r *gin.RouterGroup) {
 	r.GET("/:center", getCenter)
 	r.GET("/:center/:id", getAirport)
