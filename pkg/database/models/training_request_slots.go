980e026e283d568adb0ea47558700901b080f08f
Daniel Hawton <daniel@hawton.org>
2023-04-21 22:34:41 -0600

    switch to google uuid

--- pkg/database/models/training_request_slots.go
+++ pkg/database/models/training_request_slots.go
@@ -7,9 +7,16 @@ import (
 )
 
 type TrainingRequestSlot struct {
-	UUIDBase
+	ID                uuid.UUID        `json:"id" gorm:"primary_key;type:char(36);not null;unique_index"`
 	TrainingRequest   *TrainingRequest `json:"training_request" gorm:"foreignKey:TrainingRequestID;references:ID"`
 	TrainingRequestID uuid.UUID        `json:"-" gorm:"not null"`
 	StartTime         *time.Time       `json:"start_time" gorm:"not null"`
 	EndTime           *time.Time       `json:"end_time" gorm:"not null"`
+	CreatedAt         *time.Time       `json:"created_at"`
+	UpdatedAt         *time.Time       `json:"updated_at"`
+}
+
+func (t *TrainingRequestSlot) BeforeCreate() (err error) {
+	t.ID = uuid.New()
+	return nil
 }
