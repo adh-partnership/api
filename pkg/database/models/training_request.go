1342791a2efff1d94ed91d4f6dbda1a0d32985a4
Daniel Hawton <daniel@hawton.org>
2023-04-21 22:59:04 -0600

    lint

--- pkg/database/models/training_request.go
+++ pkg/database/models/training_request.go
@@ -12,15 +12,15 @@ import (
 
 type TrainingRequest struct {
 	ID              uuid.UUID              `json:"id" gorm:"primary_key;type:char(36);not null;unique_index"`
-	Student         *User                  `json:"student" gorm:"foreignKey:StudentID;references:ID"`
+	Student         *User                  `json:"student" gorm:"foreignKey:StudentID"`
 	StudentID       uint                   `json:"-" gorm:"not null"`
 	Position        string                 `json:"position" gorm:"not null"`
 	Status          string                 `json:"status" gorm:"not null"`
 	Notes           string                 `json:"notes"`
-	Instructor      *User                  `json:"instructor" gorm:"foreignKey:InstructorID;references:ID"`
+	Instructor      *User                  `json:"instructor" gorm:"foreignKey:InstructorID"`
 	InstructorID    *uint                  `json:"-" gorm:"default:null"`
 	InstructorNotes string                 `json:"instructor_notes"`
-	Slots           []*TrainingRequestSlot `json:"slots" gorm:"foreignKey:TrainingRequestID;references:ID"`
+	Slots           []*TrainingRequestSlot `json:"slots" gorm:"foreignKey:TrainingRequestID"`
 	Start           *time.Time             `json:"start" gorm:"default:null"`
 	End             *time.Time             `json:"end" gorm:"default:null"`
 	CreatedAt       *time.Time             `json:"created_at"`
