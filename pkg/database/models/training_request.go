bc531fbec4d8f1669e0d39701d4244ac7d2299ce
Daniel Hawton <daniel@hawton.org>
2023-04-21 22:34:41 -0600

    switch to google uuid

--- pkg/database/models/training_request.go
+++ pkg/database/models/training_request.go
@@ -5,10 +5,11 @@ import (
 
 	"github.com/adh-partnership/api/pkg/config"
 	"github.com/adh-partnership/api/pkg/database/models/constants"
+	"github.com/google/uuid"
 )
 
 type TrainingRequest struct {
-	UUIDBase
+	ID              uuid.UUID              `json:"id" gorm:"primary_key;type:char(36);not null;unique_index"`
 	Student         *User                  `json:"student" gorm:"foreignKey:StudentID;references:ID"`
 	StudentID       uint                   `json:"-" gorm:"not null"`
 	Position        string                 `json:"position" gorm:"not null"`
@@ -20,6 +21,13 @@ type TrainingRequest struct {
 	Slots           []*TrainingRequestSlot `json:"slots" gorm:"foreignKey:TrainingRequestID;references:ID"`
 	Start           *time.Time             `json:"start" gorm:"default:null"`
 	End             *time.Time             `json:"end" gorm:"default:null"`
+	CreatedAt       *time.Time             `json:"created_at"`
+	UpdatedAt       *time.Time             `json:"updated_at"`
+}
+
+func (t *TrainingRequest) BeforeCreate() (err error) {
+	t.ID = uuid.New()
+	return nil
 }
 
 func IsValidPosition(pos string) bool {
