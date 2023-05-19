9b81d07e8eb3d3eb7e04af485b1c28c8bb5642d6
Daniel Hawton <daniel@hawton.org>
2023-05-19 00:21:21 -0600

    don't add cc or bcc if blank

--- pkg/email/email.go
+++ pkg/email/email.go
@@ -124,8 +124,12 @@ func Send(to, from, subject string, template string, data map[string]interface{}
 	m := gomail.NewMessage()
 	m.SetHeader("From", from)
 	m.SetHeader("To", strings.Split(to, ", ")...)
-	m.SetHeader("Cc", strings.Split(cc, ", ")...)
-	m.SetHeader("Bcc", strings.Split(bcc, ", ")...)
+	if len(cc) > 0 {
+		m.SetHeader("Cc", strings.Split(cc, ", ")...)
+	}
+	if len(bcc) > 0 {
+		m.SetHeader("Bcc", strings.Split(bcc, ", ")...)
+	}
 	m.SetHeader("Subject", subject)
 	m.SetBody("text/html", body.String())
 
